082ee6f5274c6d919c3c31240ed551ff735b7062
Mahmoud95Khalil <Mahmoud95Khaleil@gmail.com>
2023-08-11 18:43:12 -0700

    "3-ptint_alphabet.c"

---
+++ 0x01-variables_if_else_while/3-ptint_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -lowercase and uppercase alphabets
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+
+{
+char ch = 'a';
+char Ch = 'A';
+
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (ch <= 'z')
+{
+putchar(Ch);
+Ch++;
+}
+putchar('\n');
+return (0);
+}
