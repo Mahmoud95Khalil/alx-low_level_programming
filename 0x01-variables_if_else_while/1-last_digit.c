fe8c331aae901fa84c2183649c42794a3e4061b6
Mahmoud95Khalil <Mahmoud95Khaleil@gmail.com>
2023-08-11 17:51:35 -0700

    "1-last_digit.c" git push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 digit = n % 10;
 if (digit > 5)
-printif("Last digit of" n "is" digit "and is greater than 5\n");
+printf("Last digit of" n "is" digit "and is greater than 5"), \n;
 else if (digit == 0)
-printf("Last digit of" n "is" digit "and is 0\n");
+printf("Last digit of" n "is" digit "and is 0"), \n;
 else
-printf("Last digit of" n "is" digit "and is less than 6 and not 0\n");
+printf("Last digit of" n "is" digit "and is less than 6 and not 0"), \n;
 return (0);
 }
