0875585d785e7b67bd5a321abb543a59d5390c92
Mahmoud95Khalil <Mahmoud95Khaleil@gmail.com>
2023-08-13 06:16:03 -0700

    "101-quote.c"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -A c program
+ * main - A c program to print line
+ *
  * Return 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
